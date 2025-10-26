0b507fe6970d2fd1636a785b092f6620ba2be721
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-10-26 15:18:34 +0000

    add carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,62 @@
 import Image from "next/image";
 import SocialButtons from "./components/buttons/SocialButtons";
+import Carousel from "./components/carousel/Carousel";
 
 export default function Home() {
+  const projects = [
+    {
+      title: "",
+      description: "",
+      imageUrl: "",
+      projectUrl: "",
+    },
+    {
+      title: "",
+      description: "",
+      imageUrl: "",
+      projectUrl: "",
+    },
+    {
+      title: "",
+      description: "",
+      imageUrl: "",
+      projectUrl: "",
+    },
+  ];
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+      <main className="flex min-h-screen w-full max-w-3xl flex-col pt-8 pb-32 px-16 bg-white dark:bg-black">
+        <div className="flex flex-col items-center sm:items-start">
+          <SocialButtons
+            githubUrl=""
+            linkedInUrl=""
+            emailUrl=""
+            cvUrl=""
+          />
+          <div className="w-full border-t border-black/[.08] dark:border-white/[.145] my-4"></div>
+        </div>
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            conrad tondryk
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="max-w-md text-sm leading-6 text-black  ">
+            rust, next.js, react.
+
+          </p>
+          <p className="max-w-md text-sm leading-6 text-black ">
+            i'm a software engineer, experienced in rust and next.js. check out my projects below, or contact me if you need my services.
           </p>
         </div>
-        <SocialButtons
-          githubUrl=""
-          linkedInUrl=""
-          emailUrl=""
-          cvUrl=""
-        />
+
+        <div className="mt-12">
+          <Carousel
+            projects={projects}
+            width="w-full"
+            height="h-64"
+          />
+        </div>
       </main>
     </div>
   );
